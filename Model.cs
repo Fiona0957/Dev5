5b7fe8810674bf95e23abf75e311d74d2ce79779
Fiona0957 <32257271+Fiona0957@users.noreply.github.com>
2018-10-01 20:46:13 +0200

    Add files via upload

--- Model.cs
+++ Model.cs
@@ -4,73 +4,38 @@ using System;
 
 namespace Model
 {
-    public class JobContext : DbContext
+    public class MovieContext : DbContext
     {
         //this is actual entity object linked to the movies in our DB
-        public DbSet<Company> Company { get; set; }
+        public DbSet<Movie> Movies { get; set; }
         //this is actual entity object linked to the actors in our DB
-        public DbSet<Staff> Staff { get; set; }
-        public DbSet<Task> Task { get; set; }
-        public DbSet<Perform> Perform { get; set; }
+        public DbSet<Actor> Actors { get; set; }
 
         //this method is run automatically by EF the first time we run the application
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //here we define the name of our database
-            optionsBuilder.UseNpgsql("User ID=postgres;Password=;Host=localhost;Port=5432;Database=JobDB;Pooling=true;");
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=;Host=localhost;Port=5432;Database=MovieDB;Pooling=true;");
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Perform>()
-              .HasKey(t => new { t.TaskNumber, t.StaffSSN });
-            modelBuilder.Entity<Perform>()
-              .HasOne(p => p.Staff)
-              .WithMany(ta => ta.Tasks)
-              .HasForeignKey(p => p.StaffSSN);
-            modelBuilder.Entity<Perform>()
-              .HasOne(p => p.Task)
-              .WithMany(ta => ta.Staffs)
-              .HasForeignKey(p => p.TaskNumber);
-            modelBuilder.Entity<Staff>()
-                .HasKey(a => new { a.SSN });
-            modelBuilder.Entity<Company>()
-            .HasKey(a => new { a.Name });
-            modelBuilder.Entity<Task>()
-                .HasKey(a => new { a.Number });
-        }
-
     }
 
     //this is the typed representation of a movie in our project
-    public class Company
+    public class Movie
     {
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public List<Staff> Staffs { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Release { get; set; }
+        public List<Actor> Actors { get; set; }
     }
 
     //this is the typed representation of an actor in our project
-    public class Staff
+    public class Actor
     {
-        public int SSN { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Birthday { get; set; }
-        public int Phone { get; set; }
-        public string CompanyName { get; set; }
-        public virtual List<Perform> Tasks { get; set; }
-    }
-
-    public class Task
-    {
-        public int Number { get; set; }
-        public string Description { get; set; }
-        public virtual List<Perform> Staffs { get; set; }
-    }
-    public class Perform
-    {
-        public int TaskNumber { get; set; }
-        public Task Task { get; set; }
-        public int StaffSSN { get; set; }
-        public Staff Staff { get; set; }
+        public DateTime Birth { get; set; }
+        public string Gender { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,51 +1,44 @@
 ﻿using System;
-using System.Linq;
 using Model;
+using System.Linq;
 
-namespace Dev_Assignment
+namespace Dev_Practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (var db = new JobContext())
+            Projection();
+        }
+        //using (var db = new MovieContext())
+        //{
+        //Movie m = new Movie
+        //{
+        //    Title = "Divorce Italian Style",
+        //    Release = DateTime.Now,
+        //    Actors = new System.Collections.Generic.List<Actor> {
+        //new Actor{ Name = "Marcello Mastroianni",
+        //            Birth = new DateTime(1988, 8, 29),
+        //            Gender=  "Male",
+        //            },
+        //new Actor{ Name = "Daniela Rocca",
+        //            Birth = new DateTime(1986, 5, 1),
+        //            Gender=  "Female",
+        //            }
+        //    }
+        //};
+
+        //db.Add(m);        
+        static void Projection()
+        {
+            using (var db = new MovieContext())
             {
-                //Company c1 = new Company { Name = "Apple", Address = "Plein" };
-                //Company c2 = new Company { Name = "Microsoft", Address = "Straat" };
-                //Company c3 = new Company { Name = "Acer", Address = "Pad" };
-                //Company c4 = new Company { Name = "HP", Address = "Laan" };
-                //Company c5 = new Company { Name = "Asus", Address = "Singel" };
-                //db.Company.AddRange(c1, c2, c3, c4, c5);
-
-                //Staff s1 = new Staff {SSN=2345, Name = "Tom", Birthday = "23-04-1993", Phone=123456789, CompanyName="Microsoft"};
-                //Staff s2 = new Staff {SSN =3456, Name = "Jordy", Birthday = "20-12-1989", Phone=234567891, CompanyName="Microsoft" };
-                //Staff s3 = new Staff {SSN =4567, Name = "Menno", Birthday = "12-01-1999", Phone=345678912, CompanyName="HP" };
-                //Staff s4 = new Staff {SSN =5678, Name = "Julia", Birthday = "30-09-1986", Phone=456789123, CompanyName="Asus" };
-                //Staff s5 = new Staff {SSN =6789, Name = "Danilo", Birthday = "05-10-1986", Phone=567891234, CompanyName="Asus" };
-                //db.Staff.AddRange(s1, s2, s3, s4, s5);
-
-                //Task t1 = new Task { Number = 1, Description= "Create" };
-                //Task t2 = new Task { Number = 2, Description = "Update" };
-                //Task t3 = new Task { Number = 3, Description = "Delete" };
-                //Task t4 = new Task { Number = 4, Description = "Modify" };
-                //Task t5 = new Task { Number = 5, Description = "View" };
-                //db.Task.AddRange(t1, t2, t3, t4, t5);
-
-                //Perform p1 = new Perform { TaskNumber = 2, StaffSSN = 2345 };
-                //Perform p2 = new Perform { TaskNumber = 2, StaffSSN = 3456 };
-                //Perform p3 = new Perform { TaskNumber = 1, StaffSSN = 4567 };
-                //Perform p4 = new Perform { TaskNumber = 4, StaffSSN = 5678 };
-                //Perform p5 = new Perform { TaskNumber = 3, StaffSSN = 5678 };
-                //db.Perform.AddRange(p1, p2, p3, p4, p5);
-
-                var projected = from Task in db.Task from Perform in db.Perform from Staff in db.Staff where Staff.SSN == Perform.StaffSSN && Task.Number == Perform.TaskNumber  select new {Task = Task.Description, L = Staff.Name};
-
-                foreach (var item in projected)
+                var projected_movies = from movie in db.Movies let actors_of_movie = (from actor in db.Actors where actor.MovieId == movie.Id select actor) where actors_of_movie.Count() > 3 select new { Title = movie.Title, ActorsCount = actors_of_movie.Count() };
+            foreach (var item in projected_movies)
                 {
-                    Console.WriteLine("{0}-{1}", item.Task, item.L);
+                    Console.WriteLine(item.Title, item.ActorsCount);
                 }
-
-                //db.SaveChanges();
+                db.SaveChanges();
             }
         }
     }
